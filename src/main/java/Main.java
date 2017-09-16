b4c3cf0ebd070b8367d3ada3a043c83d41846947
tomerdo <tomerdo@post.bgu.ac.il>
2017-09-16 20:12:37 +0300

    warming up with git

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("The sever is running on: " + "http://localhost:4567/hello");
         get("/hello", (req, res) -> "Hello World");
     }
 }
