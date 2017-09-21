009fe6aeaf1b29b168488925791bde0d11a93aa4
assafdev <31997647+assafdev@users.noreply.github.com>
2017-09-21 19:35:07 +0300

    another step in the establishment of the infrastructure
    - added a custom data to use in the mongo db - bank.accounts.{documents}
    - added a mongo-java-driver dependency via maven
    - added the basic Account java file

---
+++ src/main/models/Account.java
@@ -0,0 +1,61 @@
+
+public class Account {
+    private int id;
+    private String area;
+    private String name;
+    private int sectionMin;
+    private int sectionMax;
+    private int moneyChannel;
+    private Account parentAccount;
+
+    public Account(int id, String area, String name, int sectionMin, int sectionMax, int moneyChannel){
+        this.id = id;
+        this.area = area;
+        this.name = name;
+        this.sectionMin = sectionMin;
+        this.sectionMax = sectionMax;
+        this.moneyChannel = moneyChannel;
+    }
+
+    private int getId(){
+        return id;
+    }
+
+    private String getArea(){
+        return area;
+    }
+
+    private String getName(){
+        return name;
+    }
+
+    private int getSectionMin(){
+        return sectionMin;
+    }
+
+    private int getSectionMax(){
+        return sectionMax;
+    }
+
+    private int getMoneyChannel(){
+        return moneyChannel;
+    }
+
+    private Account getParentAccount(){
+        return parentAccount;
+    }
+
+    private void setArea(String newArea){
+        this.area = newArea;
+    }
+
+    private void setMoneyChannel(int newMoneyChannel){
+        this.moneyChannel = newMoneyChannel;
+    }
+
+    private void setParentAccount(Account parentAccount){
+        this.parentAccount = parentAccount;
+    }
+
+
+}
